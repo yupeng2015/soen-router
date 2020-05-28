6f54a18b2fd3734ceb8d01fd9a2cf094a3ae486e
soen <970852318@qq.com>
2020-05-28 22:21:19 +0800

    feat

--- src/Provider.php
+++ src/Provider.php
@@ -5,18 +5,28 @@ namespace Soen\Router;
 
 
 use Soen\Filesystem\File;
+use Soen\Http\Message\Request;
+use Soen\Http\Message\Response;
 
 class Provider
 {
     public $routes;
-    
+    public $routeActive;
 	function __construct($routesPath)
 	{
 	    $this->parse($routesPath);
 	}
 
-	function parse(){
-        $files = new File();
+	function parse($routesPath){
+        $files = (new File())->createFilesystemIterator($routesPath);
         $files->readFilesRequire($routesPath);
+	}
+
+	function setRouteActive(Request $request, Response $response){
+		$this->routeActive = new RouteActive($request, $response);
+	}
+
+	function getRouteActive(Request $request, Response $response){
+		return $this->routeActive;
 	}
 }
